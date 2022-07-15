b32cd0067aaad7480ad3db12a26006d561bab228
Pavel Ivanchik <ivanchik.pal@gmail.com>
2022-07-15 19:53:41 +0300

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
